4a651e3e9f6a557eab6b52f73e1a5af38aa947f5
John Chapman <contact@john-chapman.net>
2016-02-26 00:36:46 +0100

    Added asserts, removed some redundant stuff from plr_def

---
+++ plr_def.cpp
@@ -0,0 +1,75 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2016 John Chapman -- http://john-chapman.net
+// This software is distributed freely under the terms of the MIT License.
+// See http://opensource.org/licenses/MIT
+////////////////////////////////////////////////////////////////////////////////
+#include "plr_def.h"
+
+#ifdef PLR_COMPILER_MSVC
+	// disable warning about vsnprintf
+	#pragma warning(disable: 4996)
+#endif
+
+#include <cstdarg> // va_list, va_start, va_end
+#include <cstdio>  // vsprintf
+
+#include <iostream> // std::cerr
+#include <sstream>  // std::stringstream
+
+static const int kAssertMsgMax = 256; // max size for message buffer in assert()
+static PLR_THREAD_LOCAL plr::AssertCallback* pfAssertCallback = &plr::DefaultAssertCallback;
+
+void plr::SetAssertCallback(AssertCallback* pfCallback) 
+{
+	pfAssertCallback = pfCallback;
+}
+
+plr::AssertCallback* plr::GetAssertCallback() {
+	return pfAssertCallback;
+}
+
+plr::AssertBehavior plr::DefaultAssertCallback(const char* e, const char* msg, const char* file, int line) 
+{
+ // use a std::stringstream to construct a local message string before making
+ // a single call to std::cerr::operator<<, to prevent interleaved output
+ // from multiple threads (C++11 global streams are thread safe)
+	std::stringstream s;
+	s << "PLR_ASSERT (\"" << file << "\", line " << line << "):\n\t";
+	if (e) s << "\'" << e << "\' ";
+	if (msg) s << msg;
+	s << std::endl;
+
+	std::cerr << s.str(); // thread safe single call to operator<<
+
+	return AssertBehavior::kBreak;
+}
+
+
+plr::AssertBehavior plr::internal::AssertAndCallback(const char* e, const char* file, int line, const char* msg, ...) 
+{
+	char buf[kAssertMsgMax];
+	if (msg != 0) {
+		va_list args;
+		va_start(args, msg);
+		vsnprintf(buf, kAssertMsgMax, msg, args);
+		va_end(args);
+	} else {
+		buf[0] = '\0';
+	}
+
+	if (pfAssertCallback != 0) {
+		return pfAssertCallback(e, buf, plr::internal::StripPath(file), line);
+	}
+	return AssertBehavior::kBreak;
+}
+
+const char* plr::internal::StripPath(const char* _path) 
+{
+	int i = 0, last = 0;
+	while (_path[i] != 0) {
+		if (_path[i] == '\\' || _path[i] == '/')
+			last = i + 1;
+		++i;
+	}
+	return &_path[last];
+}
--- plr_def.h
+++ plr_def.h
@@ -1,5 +1,145 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2016 John Chapman -- http://john-chapman.net
+// This software is distributed freely under the terms of the MIT License.
+// See http://opensource.org/licenses/MIT
+////////////////////////////////////////////////////////////////////////////////
 #ifndef plr_def_h
 #define plr_def_h
 
+#include <cstddef> // size_t
+#include <cstdint> // sized ints
+
+#if defined(__GNUC__)
+	#define PLR_COMPILER_GNU
+#elif defined(_MSC_VER)
+	#define PLR_COMPILER_MSVC
+#endif
+
+#if defined(PLR_COMPILER_GNU)
+	#define PLR_ALIGN(a) __attribute__((aligned(a)))
+#elif defined(PLR_COMPILER_MSVC)
+	#define PLR_ALIGN(a) __declspec(align(a))
+#else
+	#define PLR_ALIGN(a) alignas(a)
+#endif
+
+#if defined(PLR_COMPILER_GNU)
+	#define PLR_ALIGNOF(t) __alignof__(t)
+#elif defined(PLR_COMPILER_MSVC)
+	#define PLR_ALIGNOF(t) __alignof(t)
+#else
+	#define PLR_ALIGNOF(t) alignof(t)
+#endif
+
+#if defined(PLR_COMPILER_GNU)
+	#define PLR_THREAD_LOCAL __thread
+#elif defined(PLR_COMPILER_MSVC)
+	#define PLR_THREAD_LOCAL __declspec(thread)
+#else
+	#define PLR_THREAD_LOCAL thread_local
+#endif
+
+#if defined(PLR_COMPILER_GNU)
+	#define if_likely(e)   if ( __builtin_expect(!!(e), 1) )
+	#define if_unlikely(e) if ( __builtin_expect(!!(e), 0) )
+//#elif defined(PLR_COMPILER_MSVC)
+  // not defined for MSVC
+#else
+	#define if_likely(e)   if(!!(e))
+	#define if_unlikely(e) if(!!(e))
+#endif
+
+#ifndef __COUNTER__
+// __COUNTER__ is non-standard, so replace with __LINE__ if it's unavailable
+	#define __COUNTER__ __LINE__
+#endif
+#define PLR_TOKEN_CONCATENATE_(_t0, _t1) _t0 ## _t1
+#define PLR_TOKEN_CONCATENATE(_t0, _t1)  PLR_TOKEN_CONCATENATE_(_t0, _t1)
+#define PLR_UNIQUE_NAME(_base) PLR_TOKEN_CONCATENATE(_base, __COUNTER__)
+
+namespace plr {
+
+typedef int8_t          sint8;
+typedef uint8_t         uint8;
+typedef int16_t         sint16;
+typedef uint16_t        uint16;
+typedef int32_t         sint32;
+typedef uint32_t        uint32;
+typedef int64_t         sint64;
+typedef uint64_t        uint64;
+
+typedef std::ptrdiff_t  sint;
+typedef std::size_t     uint;
+
+
+enum class AssertBehavior
+{
+	kBreak, 
+	kContinue
+};
+
+/// Typedef for assert callbacks. See DefaultAssertCallback for a description
+/// of the functionn arguments.
+/// \ingroup plr_debug
+typedef AssertBehavior (AssertCallback)(const char* e, const char* msg, const char* file, int line);
+
+///	\return AssertCallback in use on this thread. 
+/// Default is DefaultAssertCallback().
+///	\note This may return 0, if 0 was previously passed to SetAssertCallback().
+///	\ingroup plr_debug
+AssertCallback* GetAssertCallback();
+
+///	Set the function to be called when asserts fail on this thread.
+/// Default is DefaultAssertCallback().
+///	\ingroup plr_debug
+void SetAssertCallback(AssertCallback* callback);
+
+///	Default assert callback. Print an error message to std::cerr.
+///	\param e Stringified version of the assert expression.
+///	\param msg Optional message (passed from PLR_ASSERT_MSG,PLR_VERIFY_MSG macros).
+///	\param file, line Location of the assert.
+///	\return Always AssertBehaviour::kBreak.
+///	\ingroup plr_assert
+AssertBehavior DefaultAssertCallback(const char* e, const char* msg,  const char* file,  int line);
+
+} // namespace plr
+
+
+namespace plr { namespace internal {
+
+AssertBehavior AssertAndCallback(const char* e, const char* file, int line, const char* msg, ...);
+const char* StripPath(const char* path);
+
+} } // namespace plr::internal
+
+#define PLR_UNUSED(x) do { (void)sizeof(x); } while(0)
+#ifdef PLR_DEBUG
+	#ifdef PLR_COMPILER_MSVC
+		#define PLR_BREAK() __debugbreak()
+	#else
+		#include <cstdlib>
+		#define PLR_BREAK() abort()
+	#endif
+
+	#define PLR_ASSERT_MSG(e, msg, ...) \
+		do { \
+			if_unlikely (!(e)) { \
+				if (plr::internal::AssertAndCallback(#e, __FILE__, __LINE__, msg, __VA_ARGS__) == plr::AssertBehavior::kBreak) \
+					PLR_BREAK(); \
+			} \
+		} while(0)
+
+	#define PLR_ASSERT(e)                 PLR_ASSERT_MSG(e, 0, 0)
+	#define PLR_VERIFY_MSG(e, msg, ...)   PLR_ASSERT_MSG(e, msg, __VA_ARGS__)
+	#define PLR_VERIFY(e)                 PLR_VERIFY_MSG(e, 0, 0)
+
+#else
+	#define PLR_ASSERT_MSG(e, msg, ...)   do { PLR_UNUSED(e); PLR_UNUSED(msg); } while(0)
+	#define PLR_ASSERT(e)                 do { PLR_UNUSED(e); } while(0)
+	#define PLR_VERIFY_MSG(e, msg, ...)   do { (void)(e); PLR_UNUSED(msg); } while(0)
+	#define PLR_VERIFY(e)                 (void)(e)
+#endif // PLR_DEBUG
+
+#define PLR_STATIC_ASSERT(e) { (void)sizeof(char[(e) ? 1 : -1]); }
 
 #endif // plr_def_h
