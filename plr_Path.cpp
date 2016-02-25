c689615855242056cbc12d9b53c29cf0970ac6fd
John Chapman <contact@john-chapman.net>
2016-02-26 00:36:21 +0100

    Added plr_Path

---
+++ plr_Path.cpp
@@ -0,0 +1,139 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2016 John Chapman -- http://john-chapman.net
+// This software is distributed freely under the terms of the MIT License.
+// See http://opensource.org/licenses/MIT
+////////////////////////////////////////////////////////////////////////////////
+#include "plr_Path.h"
+
+#ifdef PLR_COMPILER_MSVC
+	// most of cstring generates warnings; disable them!
+	#pragma warning(disable: 4996)
+#endif
+#include <cstring>
+
+/// \todo make these platform dependent
+static const char* kDirSeparator = "\\/";
+static const char* kExtSeparator = ".";
+
+plr::Path::Path(const char* _path)
+	: m_len(0)
+	, m_dirsCount(0)
+	, m_fname(0)
+	, m_ext(0)
+{
+	if (_path) {
+		setPath(_path);
+	} else {
+		m_buf[0] = m_pth[0] = 0;
+	}
+}
+
+//plr::Path::Path(const Path& _rhs)
+//{
+//	memcpy(this, &_rhs, sizeof(Path));
+//}
+
+void plr::Path::setPath(const char* _path)
+{
+	m_len = (int)strlen(_path); // doesn't count null terminator
+	PLR_ASSERT(m_len < PLR_MAX_PATH_LENGTH);
+	strncpy(m_pth, _path, PLR_MAX_PATH_LENGTH); // pads with zeroes
+	parse();
+}
+
+void plr::Path::setDirectory(int i, const char* _dir)
+{
+  // remove leading separators
+	while (strchr(kDirSeparator, (int)(*_dir)) != 0) {
+		++_dir;
+	}
+	replace(getDirectory(i), _dir);
+}
+
+void plr::Path::setExtension(const char* _ext)
+{
+  // remove leading separators
+	while (strchr(kExtSeparator, (int)(*_ext)) != 0) {
+		++_ext;
+	}
+	replace(getExtension(), _ext);
+}
+
+
+// PRIVATE
+
+void plr::Path::parse()
+{
+	strncpy(m_buf, m_pth, PLR_MAX_PATH_LENGTH); // pads with zeroes
+
+  // tokenize directories into list
+	while (m_dirs[m_dirsCount] = strtok(m_dirsCount ? 0 : m_buf, kDirSeparator))
+		++m_dirsCount;
+
+  // fname is tokenized as a directory, extract/decrement dir count
+	m_fname = m_dirs[--m_dirsCount];
+
+  // ext is the characters after the last ext sep, can't use strtok as we need a reverse parse
+	for (m_ext = &m_buf[m_len - 1]; m_ext != m_buf; --m_ext) {
+		if (*m_ext != 0 && strchr(kExtSeparator, (int)(*m_ext)) != 0) { // test for 0 otherwise strchr will match it
+		// ext sep was found, insert null and move m_ext forward
+			*m_ext = 0;
+			++m_ext;
+			break;
+		}
+	}
+	if (m_ext == m_buf) {
+		m_ext = 0;
+	}
+
+}
+
+void plr::Path::replace(const char* _beg, const char* _in)
+{
+	int lenIn  = (int)strlen(_in);
+	int lenOut = (int)strlen(_beg); // length of region to be replaced
+
+	int delta = lenIn - lenOut;
+	if (delta != 0) {
+	 // move characters up/down to fit new region
+		if (delta > 0) {
+			PLR_ASSERT(m_len + delta < PLR_MAX_PATH_LENGTH); // overflow
+		// delta positive, start at the end and work backwards
+			int lim = (int)(_beg - m_buf) + lenOut;
+			int i = m_len + delta - 1;
+			for (; i != lim; --i) {
+				m_buf[i] = m_buf[i - delta];
+				m_pth[i] = m_pth[i - delta];
+			}
+		} else {
+	  	 // delta negative, start at the beginning and work forwards
+			int lim = m_len + delta + 1;
+			int i = (int)(_beg - m_buf) + lenIn;
+			for (; i != lim; ++i) {
+				m_buf[i] = m_buf[i - delta];
+				m_pth[i] = m_pth[i - delta];
+			}
+		}
+	}
+
+  // replace region
+	int i = (int)(_beg - m_buf);
+	for (; *_in != 0; ++i, ++_in) {
+		m_buf[i] = *_in;
+		m_pth[i] = *_in;
+	}
+
+  // shift metadata after the replace region by -delta
+	m_len += delta;
+	for (int i = 0; i < m_dirsCount; ++i) {
+		if (m_dirs[i] > _beg) {
+			m_dirs[i] += delta;
+		}
+	}
+	if (m_fname > _beg) {
+		m_fname += delta;
+	}
+	if (m_ext > _beg) {
+		m_ext += delta;
+	}
+}
---
+++ plr_Path.h
@@ -0,0 +1,85 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2016 John Chapman -- http://john-chapman.net
+// This software is distributed freely under the terms of the MIT License.
+// See http://opensource.org/licenses/MIT
+////////////////////////////////////////////////////////////////////////////////
+#ifndef plr_Path_h
+#define plr_Path_h
+
+#include "plr_def.h"
+
+#ifdef _WIN32
+	// max path length should match MAX_PATH (minwindef.h)
+	#define PLR_MAX_PATH_LENGTH 260
+	// max path dirs is arbitrary but should be as small as reasonbly possible
+	#define PLR_MAX_PATH_DIRS 16
+#else
+#endif
+
+namespace plr {
+
+////////////////////////////////////////////////////////////////////////////////
+/// \class Path
+/// Path manipulation.
+/// No memory allocation is performed, all interfaces are const char*.
+/// \ingroup plr_core
+////////////////////////////////////////////////////////////////////////////////
+class Path
+{
+public:
+	/// \param _path Raw path (copied to an internal buffer) up to
+	///    \c PLR_MAX_PATH_LENGTH chars (including the null terminator).
+	Path(const char* _path = 0);
+
+	/// Copy ctor.
+	//Path(const Path& _rhs);
+
+	/// Re-initialize from _path.
+	void setPath(const char* _path);
+
+	/// Replace the specified directory (leading separators are ignored)
+	void setDirectory(int i, const char* _dir);
+
+	/// Replace the filename.
+	void setFileName(const char* _name)      { replace(getFileName(), _name); }
+
+	/// Replace the extension (leading separators are ignored).
+	void setExtension(const char* _ext);
+
+	/// Implicit conversion to const char*.
+	operator const char*() const { return getPath(); }
+
+
+	int getLength() const                    { return m_len; }
+	const char* getEnd() const               { return &m_buf[m_len]; }
+
+	const char* getPath() const              { return m_pth; }
+
+	int getDirectoryCount() const            { return m_dirsCount; }
+	const char* getDirectory(int i) const    { PLR_ASSERT(i < m_dirsCount); return m_dirs[i];  }
+
+	const char* getFileName() const          { return m_fname; }
+	const char* getExtension() const         { return m_ext; }
+
+private:
+	char m_pth[PLR_MAX_PATH_LENGTH]; // original, raw path
+	int  m_len;
+	char m_buf[PLR_MAX_PATH_LENGTH]; // tokenized path
+
+	// the following point into m_buf
+	char* m_dirs[PLR_MAX_PATH_DIRS];
+	int   m_dirsCount;
+	char* m_fname;
+	char* m_ext;
+
+
+	// Tokenize raw path into m_buf.
+	void parse();
+
+	// Replaces the path substring starting at _beg with _in.
+	void replace(const char* _beg, const char* _in);
+};
+
+} // namespace plr
+
+#endif // plr_Path_h
