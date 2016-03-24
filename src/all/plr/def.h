968832ad2d42c348d894ea8c4eee39f19d92f85e
John Chapman <contact@john-chapman.net>
2016-03-24 08:56:36 +0100

    def: explicitly import types from std::

--- src/all/plr/def.h
+++ src/all/plr/def.h
@@ -79,14 +79,14 @@
 
 namespace plr {
 
-typedef int8_t          sint8;
-typedef uint8_t         uint8;
-typedef int16_t         sint16;
-typedef uint16_t        uint16;
-typedef int32_t         sint32;
-typedef uint32_t        uint32;
-typedef int64_t         sint64;
-typedef uint64_t        uint64;
+typedef std::int8_t     sint8;
+typedef std::uint8_t    uint8;
+typedef std::int16_t    sint16;
+typedef std::uint16_t   uint16;
+typedef std::int32_t    sint32;
+typedef std::uint32_t   uint32;
+typedef std::int64_t    sint64;
+typedef std::uint64_t   uint64;
 
 typedef std::ptrdiff_t  sint;
 typedef std::size_t     uint;
