8015c397c87219ac567ad1669b48faeaee080256
John Chapman <contact@john-chapman.net>
2016-07-06 13:58:16 +0200

    Hash: added _base variants to hash functions, removed redundant comments.

--- src/all/plr/hash.cpp
+++ src/all/plr/hash.cpp
@@ -9,10 +9,10 @@
 
 using namespace plr;
 
-static const uint32 kFnv1aBase32  = 0x811C9DC5u;
-static const uint32 kFnv1aPrime32 = 0x01000193u;
-static const uint64 kFnv1aBase64  = 0xCBF29CE484222325ull;
-static const uint64 kFnv1aPrime64 = 0x100000001B3ull;
+const uint32 internal::kFnv1aBase32  = 0x811C9DC5u;
+const uint64 internal::kFnv1aBase64  = 0xCBF29CE484222325ull;
+static const uint32 kFnv1aPrime32    = 0x01000193u;
+static const uint64 kFnv1aPrime64    = 0x100000001B3ull;
 
 uint16 internal::Hash16(const uint8* _buf, uint _bufSize)
 {
@@ -21,11 +21,18 @@ uint16 internal::Hash16(const uint8* _buf, uint _bufSize)
 	uint32 ret = internal::Hash32(_buf, _bufSize);
 	return (ret >> 16) ^ (((uint32)1u << 16) - 1);
 }
-
-uint32 internal::Hash32(const uint8* _buf, uint _bufSize)
+uint16 internal::Hash16(const uint8* _buf, uint _bufSize, uint16 _base)
 {
 	PLR_ASSERT(_buf);
-	uint32 ret = kFnv1aBase32;
+ // xor-folded 32-bit Hash
+	uint32 ret = internal::Hash32(_buf, _bufSize, (uint32)_base);
+	return (ret >> 16) ^ (((uint32)1u << 16) - 1);
+}
+
+uint32 internal::Hash32(const uint8* _buf, uint _bufSize, uint32 _base)
+{
+	PLR_ASSERT(_buf);
+	uint32 ret = _base;
 	const uint8* lim = _buf + _bufSize;
 	while (_buf < lim) {
 		ret ^= (uint32)*_buf++;
@@ -34,10 +41,10 @@ uint32 internal::Hash32(const uint8* _buf, uint _bufSize)
 	return ret;
 }
 
-uint64 internal::Hash64(const uint8* _buf, uint _bufSize)
+uint64 internal::Hash64(const uint8* _buf, uint _bufSize, uint64 _base)
 {
 	PLR_ASSERT(_buf);
-	uint64 ret = kFnv1aBase64;
+	uint64 ret = _base;
 	const uint8* lim = _buf + _bufSize;
 	while (_buf < lim) {
 		ret ^= (uint64)*_buf++;
@@ -53,11 +60,18 @@ uint16 internal::HashString16(const char* _str)
 	uint32 ret = internal::HashString32(_str);
 	return (ret >> 16) ^ (((uint32)1u << 16) - 1);
 }
-
-uint32 internal::HashString32(const char* _str)
+uint16 internal::HashString16(const char* _str, uint16 _base)
 {
 	PLR_ASSERT(_str);
-	uint32 ret = kFnv1aBase32;
+ // xor-folded 32-bit Hash
+	uint32 ret = internal::HashString32(_str, (uint32)_base);
+	return (ret >> 16) ^ (((uint32)1u << 16) - 1);
+}
+
+uint32 internal::HashString32(const char* _str, uint32 _base)
+{
+	PLR_ASSERT(_str);
+	uint32 ret = _base;
 	while (*_str) {
 		ret ^= (uint32)*_str++;
 		ret *= kFnv1aPrime32;
@@ -65,10 +79,10 @@ uint32 internal::HashString32(const char* _str)
 	return ret;
 }
 
-uint64 internal::HashString64(const char* _str)
+uint64 internal::HashString64(const char* _str, uint64 _base)
 {
 	PLR_ASSERT(_str);
-	uint64 ret = kFnv1aBase64;
+	uint64 ret = _base;
 	while (*_str) {
 		ret ^= (uint64)*_str++;
 		ret *= kFnv1aPrime64;
--- src/all/plr/hash.h
+++ src/all/plr/hash.h
@@ -11,78 +11,49 @@
 
 namespace plr { namespace internal {
 
-/// \return 16-bit hash of _bufSize bytes from buf.
-/// \internal \ingroup plr_core
+extern const uint32 kFnv1aBase32;
+extern const uint64 kFnv1aBase64;
+
 uint16 Hash16(const uint8* _buf, uint _bufSize);
+uint16 Hash16(const uint8* _buf, uint _bufSize, uint16 _base);
+uint32 Hash32(const uint8* _buf, uint _bufSize, uint32 _base = kFnv1aBase32);
+uint64 Hash64(const uint8* _buf, uint _bufSize, uint64 _base = kFnv1aBase64);
 
-/// \return 32-bit hash of _bufSize bytes from _buf.
-/// \internal \ingroup plr_core
-uint32 Hash32(const uint8* _buf, uint _bufSize);
-
-/// \return 64-bit hash of _bufSize bytes from _buf.
-/// \internal \ingroup plr_core
-uint64 Hash64(const uint8* _buf, uint _bufSize);
-
-/// \return 16-bit hash of null-terminated string.
-/// \internal \ingroup plr_core
 uint16 HashString16(const char* _str);
-
-/// \return 32-bit hash of null-terminated string.
-/// \internal \ingroup plr_core
-uint32 HashString32(const char* _str);
-
-/// \return 64-bit hash of null-terminated string.
-/// \internal \ingroup plr_core
-uint64 HashString64(const char* _str);
+uint16 HashString16(const char* _str, uint16 _base);
+uint32 HashString32(const char* _str, uint32 _base = kFnv1aBase32);
+uint64 HashString64(const char* _str, uint64 _base = kFnv1aBase64);
 
 } } // namespace plr::internal
 
 
 namespace plr {
 
+/// \return hash of _bufSize bytes from _buf. _base is used to initialize the result.
+/// \tparam tType type of the returned hash (only supports uint16, uint32, uint64).
+/// \ingroup plr_core
+template <typename tType>
+tType Hash(const void* _buf, uint _bufSize, tType _base);
+	template <> inline uint16 Hash<uint16>(const void* _buf, uint _bufSize, uint16 _base) { return internal::Hash16((const uint8*)_buf, _bufSize, _base); }
+	template <> inline uint32 Hash<uint32>(const void* _buf, uint _bufSize, uint32 _base) { return internal::Hash32((const uint8*)_buf, _bufSize, _base); }
+	template <> inline uint64 Hash<uint64>(const void* _buf, uint _bufSize, uint64 _base) { return internal::Hash64((const uint8*)_buf, _bufSize, _base); }
 /// \return hash of _bufSize bytes from _buf.
-/// \tparam tType type of the returned hash (only 16-, 32- and 64-bit unsigned
-///    integers are supported).
+/// \tparam tType type of the returned hash (only supports uint16, uint32, uint64).
 /// \ingroup plr_core
 template <typename tType>
 tType Hash(const void* _buf, uint _bufSize);
-	template <>
-	inline uint16 Hash<uint16>(const void* _buf, uint _bufSize)
-	{
-		return internal::Hash16((const uint8*)_buf, _bufSize);
-	}
-	template <>
-	inline uint32 Hash<uint32>(const void* _buf, uint _bufSize)
-	{
-		return internal::Hash32((const uint8*)_buf, _bufSize);
-	}
-	template <>
-	inline uint64 Hash<uint64>(const void* _buf, uint _bufSize)
-	{
-		return internal::Hash64((const uint8*)_buf, _bufSize);
-	}
+	template <> inline uint16 Hash<uint16>(const void* _buf, uint _bufSize) { return internal::Hash16((const uint8*)_buf, _bufSize); }
+	template <> inline uint32 Hash<uint32>(const void* _buf, uint _bufSize) { return internal::Hash32((const uint8*)_buf, _bufSize); }
+	template <> inline uint64 Hash<uint64>(const void* _buf, uint _bufSize) { return internal::Hash64((const uint8*)_buf, _bufSize); }
 
 /// \return hash of of null-terminated string.
-/// \tparam tType type of the returned hash (only 16-, 32- and 64-bit unsigned
-///    integers are supported).
+/// \tparam tType type of the returned hash (only supports uint16, uint32, uint64).
 /// \ingroup plr_core
 template <typename tType>
 tType HashString(const char* _str);
-	template <>
-	inline uint16 HashString<uint16>(const char* _str)
-	{
-		return internal::HashString16(_str);
-	}
-	template <>
-	inline uint32 HashString<uint32>(const char* _str)
-	{
-		return internal::HashString32(_str);
-	}
-	template <>
-	inline uint64 HashString<uint64>(const char* _str)
-	{
-		return internal::HashString64(_str);
-	}
+	template <> inline uint16 HashString<uint16>(const char* _str) { return internal::HashString16(_str); }
+	template <> inline uint32 HashString<uint32>(const char* _str) { return internal::HashString32(_str); }
+	template <> inline uint64 HashString<uint64>(const char* _str) { return internal::HashString64(_str); }
 
 } // namespace plr
 
