f31dac97b72d9ec1f733ea66526b7b5d538a680f
John <j.x.chapman@gmail.com>
2016-09-06 17:31:31 +0200

    Added MemoryPool, Pool

---
+++ src/all/plr/MemoryPool.cpp
@@ -0,0 +1,100 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2016 John Chapman -- http://john-chapman.net
+// This software is distributed freely under the terms of the MIT License.
+// See http://opensource.org/licenses/MIT
+////////////////////////////////////////////////////////////////////////////////
+#include <plr/MemoryPool.h>
+
+#include <plr/memory.h>
+
+#include <algorithm> // std::copy
+#include <new>
+
+using namespace plr;
+
+// PUBLIC
+
+MemoryPool::MemoryPool(uint _objectSize, uint _objectAlignment, uint _blockSize)
+	: m_objectSize(_objectSize)
+	, m_objectAlignment(_objectAlignment)
+	, m_blockSize(_blockSize)
+	, m_nextFree(0)
+	, m_usedCount(0)
+	, m_blocks(0)
+	, m_blockCount(0)
+{
+	PLR_ASSERT(m_objectSize >= sizeof(void*)); // objects must be at least the size of a ptr
+}
+
+MemoryPool::~MemoryPool()
+{
+	PLR_ASSERT(m_usedCount == 0); // not all objects were freed
+	for (uint i = 0; i < m_blockCount; ++i) {
+		plr::free_aligned(m_blocks[i]);
+	}
+	delete[] m_blocks;
+}
+
+void* MemoryPool::alloc()
+{
+	if (m_nextFree == 0) {
+		allocBlock();
+	}
+	void* ret = m_nextFree;
+	m_nextFree = *((void**)m_nextFree);
+	++m_usedCount;
+	return ret;
+}
+
+void MemoryPool::free(void* _object)
+{
+	PLR_ASSERT(_object);
+	PLR_ASSERT(m_usedCount > 0);
+	*((void**)_object) = m_nextFree;
+	m_nextFree = _object;
+	--m_usedCount;
+}
+
+
+// PRIVATE
+
+void MemoryPool::allocBlock()
+{
+	// allocate a new block list
+	void** tmp = new(std::nothrow) void*[m_blockCount + 1];
+	if (m_blocks) {
+		std::copy(m_blocks, m_blocks + m_blockCount, tmp);
+		delete[] m_blocks;
+	}
+	m_blocks = tmp;
+	
+	// allocate new block at list end
+	m_blocks[m_blockCount] = plr::malloc_aligned(m_objectSize * m_blockSize, m_objectAlignment);
+
+	// init free ptrs; if m_nextFree initially points to locX, after initializing
+	// the new block (starting new0) is initialized as follows:
+	//
+	// m_nextFree -> new0 -> new1 -> new2 -> new3 -> locX
+	//
+	uint p = (uint)m_blocks[m_blockCount];
+	for (uint i = 0, n = m_blockSize - 1; i < n; ++i) {
+		*((uint*)p) = p + m_objectSize;
+		p += m_objectSize;
+	}
+	*((uint*)p) = (uint)m_nextFree;
+	m_nextFree = m_blocks[m_blockCount];
+	
+	++m_blockCount;
+}
+
+bool MemoryPool::validate() const
+{
+	uint freeCount = 0;
+	void* p = m_nextFree;
+	while (p != 0) {
+		++freeCount;
+		p = *((void**)p);
+	}
+	uint totalObjects = m_blockSize * m_blockCount;
+	return m_usedCount == totalObjects - freeCount;
+}
