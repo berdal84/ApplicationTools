dafc971e7f872d4da5456c5915dc5989ee46a6a3
John Chapman <contact@john-chapman.net>
2017-04-01 20:54:11 +0200

    Added missing file

---
+++ src/all/apt/Time.cpp
@@ -0,0 +1,24 @@
+#include <apt/Time.h>
+
+#include <apt/String.h>
+
+using namespace apt;
+
+const char* Timestamp::asString() const
+{
+	static String<64> s_buf;
+	s_buf.clear();
+	float x = (float)asSeconds();
+	if (x >= 1.0f) {
+		s_buf.setf("%1.3fs", x);
+	} else {
+		x = (float)asMilliseconds();
+		if (x >= 0.1f) {
+			s_buf.setf("%1.2fms", x);
+		} else {
+			x = (float)asMicroseconds();
+			s_buf.setf("%1.0fus", x);
+		}
+	}
+	return s_buf;
+}
